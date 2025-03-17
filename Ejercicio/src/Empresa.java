2181c487cc1c3251b3e04ac41544e62623f137a2
Kevin.Velez <135194017+Kewinv@users.noreply.github.com>
2025-03-17 11:40:59 -0500

    Add files via upload

---
+++ Ejercicio/src/Empresa.java
@@ -0,0 +1,75 @@
+
+public class Empresa
+{
+
+    private static Empleado vecEmp[];
+    private static int n; 
+    public static void main(String[] args) throws Exception 
+    {
+       //Invocar todos los metodos estaticos para solucionar el problema
+
+       ingresarEmpleado();
+       nomina();
+       Consola.imprimir("\nEl salario promedio es: $" + salarioProm());
+       Consola.imprimir("\nEl porcentaje de empleados que trabajaron mas de 40 horas es: " + porcEM40h() + "%");
+    }
+
+    public static void ingresarEmpleado()
+    {  
+        Consola.imprimir("Ingrese la cantidad de empleados: ");
+        n = Consola.leerEntero();
+        //Reservar memoria (Dimensionar el vector)
+        vecEmp = new Empleado[n];
+        //Llenar el vector (Ingresar datos)
+        for (int i = 0; i <n; i++)
+        {
+            vecEmp[i] = new Empleado();
+            Consola.imprimir("\nEmpleado #" + (i + 1));
+            Consola.imprimir("\n\tDigite el nombre del empleado: ");
+            vecEmp[i].setNom(Consola.leerTexto());
+
+            Consola.imprimir("\tDigite el numero de horas trabajadas: ");
+            vecEmp[i].setNht(Consola.leerEntero());
+
+            Consola.imprimir("\tDigite el costo de la hora: ");
+            vecEmp[i].setCostoHora(Consola.leerReal());
+        }
+        
+    }
+
+    public static void nomina()
+    {
+        Consola.imprimir("NOMBRE DEL EMPLEADO\t\t\t\t\t SALARIO\n");
+        for (int i = 0; i <n; i++)
+        {
+            Consola.imprimirLn(vecEmp[i].getNom() + "\t\t\t\t\t\t\t" + vecEmp[i].calcularSalario());
+        }
+
+    }
+
+    public static double salarioProm()
+    {
+        double sueldoProm , sumaSalarios = 0;
+        for (int i = 0; i <n; i++)
+        {
+            sumaSalarios += vecEmp[i].calcularSalario();
+        }
+        sueldoProm = (sumaSalarios / n);
+        return sueldoProm;
+    }
+
+    public static double porcEM40h()
+    {
+        double porc , contadorEm40 = 0;
+        for (int i = 0; i <n; i++)
+        {
+            if(vecEmp[i].getNht()>40)
+            {
+                contadorEm40++;
+            }
+        }
+        porc = (contadorEm40 / n * 100);
+        return porc;
+       
+    }
+}
