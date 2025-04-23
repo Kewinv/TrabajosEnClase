59ac80ca382e78e1c450c957f0124265d1f480d9
Kevin.Velez <135194017+Kewinv@users.noreply.github.com>
2025-04-23 11:32:28 -0500

    Add files via upload
    Arreglo version 2 con switch e implementacion de colores en texto

--- Arreglos/src/App.java
+++ Arreglos/src/App.java
@@ -3,9 +3,67 @@ public class App
     private static int vec[]; //Declaración del arreglo
     public static void main(String[] args) throws Exception 
     {
-        llenarVector();
-        mostrarVector();
-        int pos = busquedaSecuencial(0);
+     final String CYAN = "\u001B[36m";
+     final String GREEN="\033[32m";
+     final String RED="\033[31m";
+     final String RESET = "\u001B[0m";
+        boolean sw = false; //Indica que el vector esta vacio
+        int opcion = 0;
+        do
+        {
+            Consola.imprimirLn("\n--------------------------------------------------------------------------------------------------------------------------------");
+            Consola.imprimirLn(GREEN+ " \t\t\tMENU" + RESET);
+            Consola.imprimirLn("1. Llenar el vector con numeros aleatorios\n2. Mostrar el vector\n3. Buscar un elemento\n4. Mostrar la cantidad de veces que se repite un numero\n5. Finalizar");
+            System.out.println("¿Cual es su opción?");
+            opcion = Consola.leerEntero();
+            if(opcion <=0 || opcion >5) //El "or" en java es con " || "
+            {
+                System.out.println(RED + "Numero no permitido, ingresa de nuevo" + RESET);
+            }else{
+                switch (opcion) 
+                {
+                    case 1:
+                        llenarVector();
+                        Consola.imprimirLn("El vector se ha llenado");
+                        sw = true;
+                        break;
+                        
+
+                    case 2:
+                    if(!sw)
+                    { 
+                        System.out.println(CYAN+ "No hay datos para mostrar" + RESET);
+                        break;
+                    }
+                    else
+                    {
+                        mostrarVector();
+                    }
+                        
+                        
+                    case 3:
+                    if(!sw){System.out.println(CYAN + "No hay datos para mostrar" + RESET); break;}
+                    else
+                    {
+                        Consola.imprimirLn("Ingrese el dato a buscar");
+                        int dato = Consola.leerEntero();
+                        int pos = busquedaSecuencial(dato);
+                        if(pos == -1){System.out.println(RED+"El dato no existe"+RESET);}
+                        else{System.out.println("El dato fue hallado por primera vez en la posicion " + pos);}
+                        break;
+                    }
+                        
+                        
+                
+                    default:
+                        break;
+                }
+            }
+            
+           
+            
+
+        }while(opcion != 5);
     }
 
 
@@ -25,13 +83,19 @@ public class App
     {  
         for(int i = 0; i < vec.length; i++)
         {
-            System.out.println(vec[i]);
+            System.out.print(vec[i]+" ");
         }
     }
 
     static int busquedaSecuencial(int x)
     {
-        int posicion = 0;
+        int posicion = -1; //Para indicar que el dato no existe
+        int i = 0; // Para recorrer el vector
+        while (i < vec.length && posicion == -1)
+        {
+            if(vec[i] == x){posicion = i;}
+            else{i++;}    
+        }
         return posicion;
     }
 
